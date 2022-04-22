d4304d62c9a4283c9b647a4fc43619f6723a1cbb
Justin Gilman <eyeofmidas@gmail.com>
2022-04-22 13:48:47 -0700

    getting seeded random working, using hsl for coloring, adding in keyboard controls to view generated texture

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-let bounds = {w: 128, h: 128}
+let bounds = {w: 100, h: 100}
 let data = new Array(bounds.w * bounds.h)
+let wrapAround = true
+let movement = 0
+
+let camera = {x: 0, y: 0, speed: 4}
+let keys = {}
 
 let canvas = document.getElementsByTagName('canvas')[0]
 canvas.width = bounds.w
@@ -7,20 +12,26 @@ canvas.height = bounds.h
 let context = canvas.getContext('2d')
 context.width = bounds.w
 context.height = bounds.h
-requestAnimationFrame(animate)
 function animate() {
-
-    for(let i = 0; i < data.length; i++) {
-        let position = {x:i % bounds.w,y:Math.floor(i / bounds.w)}
-        let noiseResult = bounds.w * noise(position)
-        data[i] = [noiseResult, noiseResult, noiseResult, 1]
+    if(keys["w"] || keys["ArrowUp"]) {
+        camera.y -= camera.speed
     }
-
+    if(keys["a"] || keys["ArrowLeft"]) {
+        camera.x -= camera.speed
+    }
+    if(keys["s"] || keys["ArrowDown"]) {
+        camera.y += camera.speed
+    }
+    if(keys["d"] || keys["ArrowRight"]) {
+        camera.x += camera.speed
+    }
+    let dataCopy = [...data]
     context.clearRect(0,0,bounds.w, bounds.h)
-    for(let i = 0; i < data.length; i++){
-        let pixel = data[i]
+    for(let i = 0; i < dataCopy.length; i++){
+        let pixel = dataCopy[i]
         let position = {x:i % bounds.w,y:Math.floor(i / bounds.w)}
-        context.fillStyle = `rgba(${pixel[0]},${pixel[1]},${pixel[2]},${pixel[3]})`
+        context.fillStyle = `hsla(${360 * pixel},${100}%,${50}%,${1})`
+        // context.fillStyle = `rgba(${256 * pixel},${256 * pixel},${256 * pixel},${1})`
         context.beginPath()
         context.rect(position.x,position.y, 1,1)
         context.fill()
@@ -28,8 +39,51 @@ function animate() {
     requestAnimationFrame(animate)
 }
 
+async function processFrame() {
+    for(let i = 0; i < data.length; i++) {
+        let position = {x:camera.x + i % bounds.w,y:camera.y + Math.floor(i / bounds.w)}
+        let noiseResult = perlinNoise(position, bounds)
+        // console.log(noiseResult)
+        data[i] = noiseResult
+    }
+}
 
-function noise(position){
+window.addEventListener('keydown', (event) => {
+    console.log(event)
+    keys[event.key] = true
+})
+
+window.addEventListener('keyup', (event) => {
+    keys[event.key] = false
+})
+
+
+function run() {
+    setTimeout(() => {processFrame(); run()}, 1)
+}
+run()
+requestAnimationFrame(animate)
+
+
+function perlinNoise(position, bounds) {
+    let r = 0
+    let amplitude = 128
+    let octaves = 6
+    let smoothness = 128
+    let persistance = 0.6
+    for(let i = 0; i < octaves; i++) {
+        let frequencyStep = Math.pow(2, i)
+        let amplitudeStep = Math.pow(persistance, i)
+        let modifiedPosition = {x: position.x * frequencyStep / smoothness, y: position.y * frequencyStep / smoothness}
+        r += noise(modifiedPosition, bounds) * amplitudeStep
+    }
+    let result = ((r / 2 + 1) * amplitude) / 256
+    // console.log(result)
+
+    return Math.min(1, Math.max(0, result))
+}
+
+function noise(position, bounds){
     //pull out the fractional bits of the position to use
     let integerPosition = {x:parseInt(position.x), y: parseInt(position.y)}
     let fractionalPosition = {x: position.x - integerPosition.x, y: position.y - integerPosition.y}
@@ -39,6 +93,21 @@ function noise(position){
     let topRight = {x:integerPosition.x+1, y:integerPosition.y}
     let bottomLeft = {x:integerPosition.x, y:integerPosition.y+1}
     let bottomRight = {x:integerPosition.x+1, y:integerPosition.y+1}
+    
+    if(wrapAround) {
+        if(topRight.x == bounds.w) {
+            topRight.x = 0
+        }
+        if(bottomLeft.y == bounds.h) {
+            bottomLeft.y = 0
+        }
+        if(bottomRight.x == bounds.w) {
+            bottomRight.x = 0
+        }
+        if(bottomRight.y == bounds.h) {
+            bottomRight.y = 0
+        }
+    }
 
     let a = getNoise(topLeft, bounds)
     let b = getNoise(topRight, bounds)
@@ -48,7 +117,7 @@ function noise(position){
     let topInterpolated = cosineInterpolate(a, b, fractionalPosition.x)
     let bottomInterpolated = cosineInterpolate(c, d, fractionalPosition.y)
 
-    return cosineInterpolate(topInterpolated, bottomInterpolated, fractionalPosition.x)
+    return cosineInterpolate(topInterpolated, bottomInterpolated, fractionalPosition.y)
     
 }
 
@@ -60,7 +129,7 @@ function getNoise(position, bounds){
     // return sfc32(timeSeed(),xSeed(), ySeed(), boundSeed(), )()
 
     let positionSeed = xmur3(`${position.x + position.y * bounds.w}`)
-    return mulberry32(positionSeed() + new Date().getTime())()
+    return mulberry32(positionSeed())()
 }
 
 https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
@@ -100,6 +169,6 @@ function mulberry32(a) {
 
 function cosineInterpolate(a, b, t) {
     let c = 1 - Math.cos(t * Math.PI) * 0.5
-    return (1 - c) * a + c * b
+    return (1.0 - c) * a + c * b
 }
 
